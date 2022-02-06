6ee67e7cc5a28062ec702801174cda9ae0d54883
DannyLeWasTaken <DannyLeWasTaken@protonmail.com>
2022-02-06 14:58:23 -0500

    Inital commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Reference used for simulation: https://docs.google.com/document/d/10sXEhzFRSnvFcl3XxNGhnD4N2SedqwdAvK3dsihxVUA/edit#
+
+from re import L
+import pybullet
+import time
+import pybullet_data
+
+tick = 0
+
+physicsClient = pybullet.connect(pybullet.GUI)
+pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
+pybullet.setGravity(0,0,-9.81)
+pybullet.planeId = pybullet.loadURDF("plane.urdf")
+startPos = [0,0,1]
+startOrientation = pybullet.getQuaternionFromEuler([0,0,0])
+boxId = pybullet.loadURDF("r2d2.urdf", startPos, startOrientation)
+
+pybullet.setRealTimeSimulation(1)
+
+"""
+for i in range(10000):
+	pybullet.stepSimulation()
+	a = time.sleep(1./960.)
+"""
+
+cubePos, cubeOrn = pybullet.getBasePositionAndOrientation(boxId)
+print(cubePos, cubeOrn)
+pybullet.disconnect()
