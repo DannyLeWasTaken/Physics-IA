255b5dc1a535da4368e7144bd181e2fda04a6abc
DannyLeWasTaken <DannyLeWasTaken@protonmail.com>
2022-02-08 02:27:08 -0500

    I have no clue what I did nor do I wanna know. it's 3am rn pain.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import pybullet_data
 import random
 import math
+import vector
 
 tick = 0
 
@@ -13,10 +14,6 @@ physicsClient = pybullet.connect(pybullet.GUI)
 pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 pybullet.setGravity(0,0,-9.81)
 pybullet.planeId = pybullet.loadURDF("plane.urdf")
-
-
-startPos = [0,0,5]
-startOrientation = pybullet.getQuaternionFromEuler([0,0,0])
 
 # Goal velocities of ramp
 rotations = [30,35,40,45,50]
@@ -27,15 +24,20 @@ for rotation in rotations:
 	for i in range(5):
 		# Set up simulation
 		deltaTime = time.time()
-		lastPos = startPos
+		simulationTime = 0
+		simulationStep = 0
+		forceApplied = False
+		continueSimulation = True
 		
 		inclineOrientation = pybullet.getQuaternionFromEuler((0, rotation * (math.pi/180), 0))
 
 		inclinePlaneId = pybullet.loadURDF("plane.urdf", (0,0,0), inclineOrientation)
 		pybullet.setCollisionFilterPair(inclinePlaneId, pybullet.planeId, -1, -1, 0)
 		pybullet.changeDynamics(inclinePlaneId, -1, mass=0)
-		
-		cubeId = pybullet.loadURDF("physics_block.urdf", (0,0,0), inclineOrientation)
+
+		rayResult = pybullet.rayTest((-5,0,10), (-5,0,0))
+
+		cubeId = pybullet.loadURDF("physics_block.urdf", (-5, 0, 10), inclineOrientation)
 		
 
 
@@ -43,18 +45,22 @@ for rotation in rotations:
 		quatrotation = pybullet.getQuaternionFromEuler((0,rotation,0));
 
 		# Actually simulate
-		for step in range(500):
-			pybullet.stepSimulation()
+		while continueSimulation:
 			deltaTime = time.time() - deltaTime
 
 			cubePos, cubeOrn = pybullet.getBasePositionAndOrientation(cubeId)
+
+			if simulationStep >= 500 and not forceApplied:
+				startPos = cubePos
+				pybullet.applyExternalForce(cubeId, -1, cubePos, (1,0,0), pybullet.WORLD_FRAME)
+				forceApplied = True
 			
+			if math.sqrt(cubePos[0]^2 + cubePos[1]^2 + cubePos[2]^2):
+				continueSimulation = False
 
-
-			lastPos = cubePos;
-
+			simulationTime += 1./240.
 			time.sleep(1./240.)
-		
+			pybullet.stepSimulation()
 		pybullet.removeBody(cubeId)
 		pybullet.removeBody(inclinePlaneId)
 		print("Finished simulation for 1")
