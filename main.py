9e9302e060be364e28116eddf124e0948c0849ee
DannyLeWasTaken <DannyLeWasTaken@protonmail.com>
2022-02-08 17:27:37 -0500

    Added more relavent trials

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 # CONFIGRUATION
 numberTrials = 2
-rotations = [30,35,40,60,80]
+rotations = [20,25,30,35,40,60,80]
 
 recordedData = {}
 
